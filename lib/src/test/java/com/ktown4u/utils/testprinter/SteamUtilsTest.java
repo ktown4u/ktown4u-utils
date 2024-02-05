b096631bea167f7097abe7324d969255dd1fab9c
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-05 17:45:25 +0900

    add SteamUtilsTest - test list

---
+++ lib/src/test/java/com/ktown4u/utils/testprinter/SteamUtilsTest.java
@@ -0,0 +1,17 @@
+package com.ktown4u.utils.testprinter;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * TODO:
+ * [ ] getItem_whenNotFound, 컬렉션, Predicate을 제공하면 Predicate을 만족하는 컬렉션의 원소가 없으면 예외를 발생시킨다
+ * [ ] getItem_whenFound, Predicate을 제공하면 Predicate을 만족하는 컬렉션의 원소를 반환한다
+ * [ ] fetchItems, 컬렉션, Id 추출 Function, Id 컬렉션으로 객체를 반환하는 fetch Function을 제공하면 Id 컬렉션을 추출하여 fetch Function을 실행한 결과를 반환한다
+ */
+public class SteamUtilsTest {
+    @DisplayName("컬렉션, Predicate을 제공하면 Predicate을 만족하는 컬렉션의 원소가 없으면 예외를 발생시킨다")
+    @Test
+    void getItem_whenNotFound() {
+    }
+}

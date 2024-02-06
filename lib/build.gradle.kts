32692048e64d5203c61b1d16ee32b1903f048712
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-06 17:08:49 +0900

    add version to 1.1.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -12,7 +12,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.ktown4u"
             artifactId = "utils"
-            version = "1.0.2"
+            version = "1.1.0"
 
             from(components["java"])
         }

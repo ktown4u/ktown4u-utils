2038b7ac8db0e22cc33a24c2281daaf71a0f7589
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-06 17:07:56 +0900

    extract delegate

--- lib/src/test/java/com/ktown4u/utils/testprinter/SteamUtilsTest.java
+++ lib/src/test/java/com/ktown4u/utils/testprinter/SteamUtilsTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -19,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * [X] getItemOrElseThrow_returns_found_whenFound, Predicate을 제공하면 Predicate을 만족하는 컬렉션의 원소를 반환한다
  * [X] getItem_returns_optional, Predicate을 제공하면 Predicate을 만족하는 컬렉션의 원소나 empty Optional을 반환한다
  * [X] fetchIds, 컬렉션에서 Id 추출 Function를 적용해서 Id 컬렉션을 반환한다
- * [ ] fetchItems, fetchIds를 적용해서 얻은 Id 컬렉션으로 객체를 반환하는 fetch Function을 적용해서 얻은 객체 컬렉션을 반환한다
+ * [X] fetchItems, fetchIds를 적용해서 얻은 Id 컬렉션으로 객체를 반환하는 fetch Function을 적용해서 얻은 객체 컬렉션을 반환한다
  */
 class SteamUtilsTest {
 
@@ -35,7 +33,7 @@ class SteamUtilsTest {
     @DisplayName("컬렉션, Predicate를 제공하면 조건을 만족하는 첫번째 원소를 포함하는 Optional을 반환한다")
     @Test
     void getItem_returns_optional() {
-        Optional<User> user = getItem(sourceCollection, user1 -> user1.name().equals("msbaek"));
+        Optional<User> user = StreamUtils.getItem(sourceCollection, user1 -> user1.name().equals("msbaek"));
         user.map(User::name).ifPresentOrElse(
                 name -> assertThat(name).isEqualTo("msbaek"),
                 () -> assertThat(false).isTrue()
@@ -45,63 +43,34 @@ class SteamUtilsTest {
     @DisplayName("컬렉션, Predicate을 제공하면 Predicate을 만족하는 컬렉션의 원소가 없으면 예외를 발생시킨다")
     @Test
     void getItemOrElseThrow_throws_exception_whenNotFound() {
-        assertThrows(IllegalArgumentException.class, () -> getItemOrElseThrow(sourceCollection, user -> user.name().equals("msbaek3"), errorMessage));
+        assertThrows(IllegalArgumentException.class, () -> StreamUtils.getItemOrElseThrow(sourceCollection, user -> user.name().equals("msbaek3"), errorMessage));
     }
 
     @DisplayName("컬렉션, Predicate을 제공하면 Predicate을 만족하는 컬렉션의 원소를 반환한다")
     @Test
     void getItemOrElseThrow_returns_found_whenFound() {
-        User user = getItemOrElseThrow(sourceCollection, user1 -> user1.name().equals("msbaek"), errorMessage);
+        User user = StreamUtils.getItemOrElseThrow(sourceCollection, user1 -> user1.name().equals("msbaek"), errorMessage);
         assertThat(user).isEqualTo(new User("msbaek", 1l));
     }
 
     @DisplayName("컬렉션에서 Id 추출 Function를 적용해서 Id 컬렉션을 반환한다")
     @Test
     void fetchIds_returns_id_collection() {
-        Collection<Long> ids = fetchIds(sourceCollection, User::id);
+        Collection<Long> ids = StreamUtils.fetchIds(sourceCollection, User::id);
         assertThat(ids).containsExactly(1l, 2l);
     }
 
-    /**
-     * Collection에서 조건에 맞는 첫번째 아이템을 Optional로 반환한다.
-     *
-     * @param sourceCollection stream 처리할 소스 컬렉션
-     * @param predicate        filter에 사용할 조건
-     * @param <T>              반환할 타입
-     * @return 조건에 맞는 첫번째 아이템 or empty Optional
-     */
-    public static <T> Optional<T> getItem(final Collection<T> sourceCollection, final Predicate<T> predicate) {
-        return sourceCollection.stream()
-                .filter(predicate)
-                .findFirst();
+    @DisplayName("fetchIds를 적용해서 얻은 Id 컬렉션으로 객체를 반환하는 fetch Function을 적용해서 얻은 객체 컬렉션을 반환한다")
+    @Test
+    void fetchItems_returns_items_collection() {
+        Collection<Long> ids = StreamUtils.fetchIds(sourceCollection, User::id);
+        Collection<User> users = StreamUtils.fetchItems(sourceCollection, User::id, this::listUsersBy);
+        assertThat(users).containsExactly(new User("msbaek", 1l), new User("msbaek2", 2l));
     }
 
-    /**
-     * Collection에서 조건에 맞는 아이템을 반환한다.
-     * 조건에 맞는 아이템이 존재하지 않으면 errorMessage를 가진 예외를 발생시킨다.
-     *
-     * @param sourceCollection stream 처리할 소스 리스트
-     * @param predicate        filter에 사용할 조건
-     * @param errorMessage     조건에 맞는 아이템이 없을 때 발생시킬 예외의 메시지
-     * @param <T>              반환할 타입
-     * @return 조건에 맞는 아이템
-     */
-    public static <T> T getItemOrElseThrow(final Collection<T> sourceCollection, final Predicate<T> predicate, final String errorMessage) {
-        return getItem(sourceCollection, predicate)
-                .orElseThrow(() -> new IllegalArgumentException(errorMessage));
-    }
-
-    /**
-     * Collection에서 Id를 추출하여 Id 컬렉션을 반환한다.
-     *
-     * @param sourceCollection stream 처리할 소스 리스트
-     * @param idExtractor      소스 리스트의 원소에서 id를 추출할 함수
-     * @param <T>              sourceList의 타입
-     * @return id 컬렉션
-     */
-    public static <T> Collection<Long> fetchIds(Collection<T> sourceCollection, Function<T, Long> idExtractor) {
+    private Collection<User> listUsersBy(Collection<Long> ids) {
         return sourceCollection.stream()
-                .map(idExtractor)
+                .filter(user -> ids.contains(user.id()))
                 .toList();
     }
 }
---
+++ lib/src/test/java/com/ktown4u/utils/testprinter/StreamUtils.java
@@ -0,0 +1,68 @@
+package com.ktown4u.utils.testprinter;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * Stream Pipeline을 사용하는 유틸리티 함수들을 모아놓은 클래스
+ */
+public class StreamUtils {
+    /**
+     * Collection에서 조건에 맞는 첫번째 아이템을 Optional로 반환한다.
+     *
+     * @param sourceCollection stream 처리할 소스 컬렉션
+     * @param predicate        filter에 사용할 조건
+     * @param <T>              반환할 타입
+     * @return 조건에 맞는 첫번째 아이템 or empty Optional
+     */
+    public static <T> Optional<T> getItem(final Collection<T> sourceCollection, final Predicate<T> predicate) {
+        return sourceCollection.stream()
+                .filter(predicate)
+                .findFirst();
+    }
+
+    /**
+     * Collection에서 조건에 맞는 아이템을 반환한다.
+     * 조건에 맞는 아이템이 존재하지 않으면 errorMessage를 가진 예외를 발생시킨다.
+     *
+     * @param sourceCollection stream 처리할 소스 리스트
+     * @param predicate        filter에 사용할 조건
+     * @param errorMessage     조건에 맞는 아이템이 없을 때 발생시킬 예외의 메시지
+     * @param <T>              반환할 타입
+     * @return 조건에 맞는 아이템
+     */
+    public static <T> T getItemOrElseThrow(final Collection<T> sourceCollection, final Predicate<T> predicate, final String errorMessage) {
+        return getItem(sourceCollection, predicate)
+                .orElseThrow(() -> new IllegalArgumentException(errorMessage));
+    }
+
+    /**
+     * Collection에서 idExtract를 적용해서 Id 컬렉션을 반환한다.
+     *
+     * @param sourceCollection stream 처리할 소스 리스트
+     * @param idExtractor      소스 리스트의 원소에서 id를 추출할 함수
+     * @param <T>              sourceList의 타입
+     * @return id 컬렉션
+     */
+    public static <T> Collection<Long> fetchIds(Collection<T> sourceCollection, Function<T, Long> idExtractor) {
+        return sourceCollection.stream()
+                .map(idExtractor)
+                .toList();
+    }
+
+    /**
+     * Collection에 fethId를 적용해서 얻은 Id 컬렉션으로 객체를 반환하는 fetch Function을 적용해서 얻은 객체 컬렉션을 반환한다.
+     *
+     * @param sourceCollection stream 처리할 소스 리스트
+     * @param idExracttor      소스 리스트의 원소에서 id를 추출할 함수
+     * @param fetchFunction    id를 받아서 데이터를 가져올 함수
+     * @param <T>              sourceList의 타입
+     * @return idExtractor가 반환한 id 값들을 갖는 객체 컬렉션
+     */
+    public static <T> Collection<T> fetchItems(Collection<T> sourceCollection, Function<T, Long> idExracttor, Function<Collection<Long>, Collection<T>> fetchFunction) {
+        Collection<Long> ids = fetchIds(sourceCollection, idExracttor);
+        return fetchFunction.apply(ids);
+    }
+}

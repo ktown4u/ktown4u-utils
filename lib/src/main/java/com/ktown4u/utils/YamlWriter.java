6ad99e2a9bdef9b6040e67d7d66d63671dc595b4
이기원/백엔드개발팀 <jake@ktown4u.com>
2024-02-10 15:14:27 +0900

    YamlWriter.write 구현.

--- lib/src/main/java/com/ktown4u/utils/YamlWriter.java
+++ lib/src/main/java/com/ktown4u/utils/YamlWriter.java
@@ -1,9 +1,30 @@
 package com.ktown4u.utils;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
 public enum YamlWriter {
     ;
+    private static final ObjectMapper mapper;
+
+    static {
+        mapper = new ObjectMapper(new YAMLFactory());
+        mapper.findAndRegisterModules();
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE); // Disable auto-detection for all methods
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY); // Enable visibility for fields
+    }
 
     public static String write(final Object object) {
-        throw new UnsupportedOperationException("Not implemented");
+        try {
+            return mapper.writeValueAsString(object);
+        } catch (final JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

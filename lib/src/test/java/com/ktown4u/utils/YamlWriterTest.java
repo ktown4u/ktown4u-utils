74fc40824259f33e6d5860d595c6a05d7c469c0a
이기원/백엔드개발팀 <jake@ktown4u.com>
2024-02-10 15:15:28 +0900

    extract field, introduce variable.

--- lib/src/test/java/com/ktown4u/utils/YamlWriterTest.java
+++ lib/src/test/java/com/ktown4u/utils/YamlWriterTest.java
@@ -10,30 +10,32 @@ import static com.ktown4u.utils.ProductBuilder.aProduct;
 
 class YamlWriterTest {
 
+    private final Order order = anOrder()
+            .orderLineItems(
+                    anOrderLineItem()
+                            .quantity(2L)
+                            .product(
+                                    aProduct()
+                                            .name("Kenya AA Drip Coffee")
+                                            .description("Bright, citrusy, with a hint of cocoa and a smooth finish.")
+                                            .price("8000")
+                            ),
+                    anOrderLineItem()
+                            .quantity(1L)
+                            .product(
+                                    aProduct()
+                                            .name("Americano")
+                                            .description("2-shot original blend Americano.")
+                                            .price("5000")
+                            )
+            )
+            .build();
+
     @Test
     @DisplayName("write - 모든 필드를 YAML 포멧 문자열로 반환.")
     void write() {
-        final Order order = anOrder()
-                .orderLineItems(
-                        anOrderLineItem()
-                                .quantity(2L)
-                                .product(
-                                        aProduct()
-                                                .name("Kenya AA Drip Coffee")
-                                                .description("Bright, citrusy, with a hint of cocoa and a smooth finish.")
-                                                .price("8000")
-                                ),
-                        anOrderLineItem()
-                                .quantity(1L)
-                                .product(
-                                        aProduct()
-                                                .name("Americano")
-                                                .description("2-shot original blend Americano.")
-                                                .price("5000")
-                                )
-                )
-                .build();
+        final String result = YamlWriter.write(order);
 
-        Approvals.verify(YamlWriter.write(order));
+        Approvals.verify(result);
     }
 }

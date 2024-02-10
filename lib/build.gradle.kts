3a8b26edcd6634e013dd3437572050fda48e5678
이기원/백엔드개발팀 <jake@ktown4u.com>
2024-02-10 15:40:14 +0900

    update artifact version.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -12,7 +12,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.ktown4u"
             artifactId = "utils"
-            version = "1.1.0"
+            version = "1.2.0"
 
             from(components["java"])
         }

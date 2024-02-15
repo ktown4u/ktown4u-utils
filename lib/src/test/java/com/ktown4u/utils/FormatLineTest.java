5554b938a2e7f9b712d2d85a33fbb09641c0130b
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-15 14:24:07 +0900

    make it work

--- lib/src/test/java/com/ktown4u/utils/FormatLineTest.java
+++ lib/src/test/java/com/ktown4u/utils/FormatLineTest.java
@@ -19,4 +19,11 @@ public class FormatLineTest {
         // then
         Approvals.verify(result);
     }
+
+
+    private String formatLineWithWhitespaces(final String name, final Object value, int columns) {
+        final int length = null == value ? 4 : value.toString().length();
+        final int whitespaceSize = columns - name.length() - length;
+        return String.format("%s%s%s\n", name, " ".repeat(Math.max(0, whitespaceSize)), value);
+    }
 }

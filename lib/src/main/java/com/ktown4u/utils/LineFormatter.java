fd63ea141132b6622af8007edab8fed868196ccd
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-15 14:28:34 +0900

    columns를 필드로 추출, setUp 추가,

--- lib/src/main/java/com/ktown4u/utils/LineFormatter.java
+++ lib/src/main/java/com/ktown4u/utils/LineFormatter.java
@@ -1,12 +1,17 @@
 package com.ktown4u.utils;
 
 public class LineFormatter {
-    public LineFormatter() {
+
+    private int columns;
+
+    public LineFormatter(int columns) {
+        this.columns = columns;
     }
 
-    String formatLineWithWhitespaces(final String name, final Object value, int columns) {
+    public String formatLineWithWhitespaces(final String name, final Object value) {
         final int length = null == value ? 4 : value.toString().length();
-        final int whitespaceSize = columns - name.length() - length;
+        this.columns = columns;
+        final int whitespaceSize = this.columns - name.length() - length;
         return String.format("%s%s%s\n", name, " ".repeat(Math.max(0, whitespaceSize)), value);
     }
 }
--- lib/src/test/java/com/ktown4u/utils/FormatLineTest.java
+++ lib/src/test/java/com/ktown4u/utils/FormatLineTest.java
@@ -1,11 +1,18 @@
 package com.ktown4u.utils;
 
 import org.approvaltests.Approvals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class FormatLineTest {
-    private final LineFormatter lineFormatter = new LineFormatter();
+    private final int columns = 10;
+    private LineFormatter lineFormatter = new LineFormatter(columns);
+
+    @BeforeEach
+    void setUp() {
+        lineFormatter = new LineFormatter(columns);
+    }
 
     @DisplayName("name, value, columns를 입력하면 name과 value 사이에 columns만큼의 공백을 추가한 문자열을 반환한다")
     @Test
@@ -13,16 +20,11 @@ public class FormatLineTest {
         // given
         final String name = "name";
         final Object value = "value";
-        final int columns = 10;
 
         // when
-        final String result = lineFormatter.formatLineWithWhitespaces(name, value, columns);
+        final String result = lineFormatter.formatLineWithWhitespaces(name, value);
 
         // then
         Approvals.verify(result);
-    }
-
-    private String formatLineWithWhitespaces(final String name, final Object value, int columns) {
-        return lineFormatter.formatLineWithWhitespaces(name, value, columns);
     }
 }

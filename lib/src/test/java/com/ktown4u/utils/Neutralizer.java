46f9113ee61d5124112e0bec2e4e51b7e7592818
Myeongseok Baek <codetemplate@hanmail.net>
2024-03-30 10:22:49 +0900

    refactor test

--- lib/src/test/java/com/ktown4u/utils/Neutralizer.java
+++ lib/src/test/java/com/ktown4u/utils/Neutralizer.java
@@ -3,7 +3,7 @@ package com.ktown4u.utils;
 import java.util.regex.Pattern;
 
 public class Neutralizer {
-    private static final String LOCAL_DATE_TIME_REPLACEMENT = "2003-05-03T10:11:12.134567";
+    public static final String LOCAL_DATE_TIME_REPLACEMENT = "2003-05-03T10:11:12.134567";
     private static final Pattern localDateTimeePattern =
             Pattern.compile("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{6}");
 
--- lib/src/test/java/com/ktown4u/utils/NeutralizerTest.java
+++ lib/src/test/java/com/ktown4u/utils/NeutralizerTest.java
@@ -9,13 +9,10 @@ public class NeutralizerTest {
     @DisplayName("문자열에 포한된 LocalDateTime의 문자열 표현을 특정 문자로 변환하여 테스트에서 매번 변경되는 LocalDateTime을 대응한다")
     @Test
     void neutralizeLocalDateTime() {
-        // Given
         String string = "GoodsFamily{id=1, name='name', createdBy=1, updatedBy=null, createdAt=2021-08-01T00:00:00.000000, updatedAt=null, goodsFamily2Goods=[]}";
 
-        // When
         String result = Neutralizer.localDateTime(string);
 
-        // Then
-        assertThat(result).contains("2003-05-03T10:11:12.134567");
+        assertThat(result).contains(Neutralizer.LOCAL_DATE_TIME_REPLACEMENT);
     }
 }

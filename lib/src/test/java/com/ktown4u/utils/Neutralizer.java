125fe6a65b33f04c847c82bfdbb56fa3370b092c
Myeongseok Baek <codetemplate@hanmail.net>
2024-03-30 10:19:31 +0900

    make it static

--- lib/src/test/java/com/ktown4u/utils/Neutralizer.java
+++ lib/src/test/java/com/ktown4u/utils/Neutralizer.java
@@ -3,14 +3,11 @@ package com.ktown4u.utils;
 import java.util.regex.Pattern;
 
 public class Neutralizer {
-    private final String LOCAL_DATE_TIME_REPLACEMENT = "2003-05-03T10:11:12.134567";
-    private final Pattern localDateTimeePattern =
+    private static final String LOCAL_DATE_TIME_REPLACEMENT = "2003-05-03T10:11:12.134567";
+    private static final Pattern localDateTimeePattern =
             Pattern.compile("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{6}");
 
-    public Neutralizer() {
-    }
-
-    String localDateTime(String string) {
+    public static String localDateTime(String string) {
         return localDateTimeePattern
                 .matcher(string)
                 .replaceAll(LOCAL_DATE_TIME_REPLACEMENT);
--- lib/src/test/java/com/ktown4u/utils/NeutralizerTest.java
+++ lib/src/test/java/com/ktown4u/utils/NeutralizerTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NeutralizerTest {
-    private final Neutralizer neutralizer = new Neutralizer();
-
     @DisplayName("문자열에 포한된 LocalDateTime의 문자열 표현을 특정 문자로 변환하여 테스트에서 매번 변경되는 LocalDateTime을 대응한다")
     @Test
     void neutralizeLocalDateTime() {
@@ -15,7 +13,7 @@ public class NeutralizerTest {
         String string = "GoodsFamily{id=1, name='name', createdBy=1, updatedBy=null, createdAt=2021-08-01T00:00:00.000000, updatedAt=null, goodsFamily2Goods=[]}";
 
         // When
-        String result = neutralizer.localDateTime(string);
+        String result = Neutralizer.localDateTime(string);
 
         // Then
         assertThat(result).contains("2003-05-03T10:11:12.134567");

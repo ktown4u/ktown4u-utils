1467f6c7568be3e4117b785da55ab8e2b615670b
Myeongseok Baek <codetemplate@hanmail.net>
2024-03-30 10:18:34 +0900

    refactor Neutralizer for readability

--- lib/src/test/java/com/ktown4u/utils/Neutralizer.java
+++ lib/src/test/java/com/ktown4u/utils/Neutralizer.java
@@ -3,13 +3,16 @@ package com.ktown4u.utils;
 import java.util.regex.Pattern;
 
 public class Neutralizer {
+    private final String LOCAL_DATE_TIME_REPLACEMENT = "2003-05-03T10:11:12.134567";
+    private final Pattern localDateTimeePattern =
+            Pattern.compile("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{6}");
+
     public Neutralizer() {
     }
 
     String localDateTime(String string) {
-        String replacement = "2003-05-03T10:11:12.134567";
-        String regex = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{6}";
-        Pattern pattern = Pattern.compile(regex);
-        return pattern.matcher(string).replaceAll(replacement);
+        return localDateTimeePattern
+                .matcher(string)
+                .replaceAll(LOCAL_DATE_TIME_REPLACEMENT);
     }
 }

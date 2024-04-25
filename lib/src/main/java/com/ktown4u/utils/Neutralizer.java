0988e90370fb71218ede72d669c055bb9351466d
Myeongseok Baek <codetemplate@hanmail.net>
2024-04-25 11:54:03 +0900

    change last digit number 6 to gte 2

--- lib/src/main/java/com/ktown4u/utils/Neutralizer.java
+++ lib/src/main/java/com/ktown4u/utils/Neutralizer.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class Neutralizer {
     public static final String LOCAL_DATE_TIME_REPLACEMENT = "2003-05-03T10:11:12.134567";
     private static final Pattern localDateTimeePattern =
-            Pattern.compile("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{6}");
+            Pattern.compile("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{2,}");
 
     public static String localDateTime(String string) {
         return localDateTimeePattern

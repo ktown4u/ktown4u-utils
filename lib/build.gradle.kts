06aec826e652cfa8693178c50cc950cc72fd99cb
박혜민/백엔드개발팀 <hyemin@ktown4u.com>
2024-07-26 15:10:29 +0900

    update version

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -12,7 +12,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.ktown4u"
             artifactId = "utils"
-            version = "1.3.0"
+            version = "1.6.0"
 
             from(components["java"])
         }
